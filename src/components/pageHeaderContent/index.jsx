b2bcd75c261365ee2163fba607d5f7bad8a15edf
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-03-15 22:04:13 +0530

    removed icon

--- src/components/pageHeaderContent/index.jsx
+++ src/components/pageHeaderContent/index.jsx
@@ -1,12 +1,12 @@
 import './styles.scss';
 
 const PageHeaderContent = (props) => {
-  const { headerText, icon } = props;
+  const { headerText } = props;
 
   return (
     <div className="wrapper">
       <h2>{headerText}</h2>
-      <span>{icon}</span>
+      
     </div>
   );
 };
