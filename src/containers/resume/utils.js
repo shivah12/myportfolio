c38d87ef4d02950102f4f33558d5b7b77dbdc803
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-17 14:39:20 +0530

    Update utils.js

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -1,15 +1,30 @@
 export const data = {
     experience: [
       {
+        title: "UI/UX Designer",
+        subTitle: "Digital Dose",
+        description:
+          " Currently working as an Intern focusing on both App and Web Designs",
+        duration: "May, 2024- Present"
+      },
+      {
         title: "Technical Team member",
         subTitle: "VSSUT Robotics",
         description:
-          " Web Development, Python",
-        duration: "2022 - Present"
+        `
+        • Developed the orientation form for the new inductees registration purposes using ReactJs and Firestore Real-time Database.
+      `,
+        duration: "April,2023- Present"
       },
     ],
     education: [
-     
+      {
+        title: "B.Tech",
+        subTitle: "Veer Surendra Sai University of technology, Burla",
+        description:
+          "Bachelor of technology in Information Technology",
+        duration:"2022 - Present"
+      },
       {
         title: "Higher Secondary",
         subTitle: "DAV Public School Chandrasekharpur, BBSR",
@@ -17,12 +32,6 @@ export const data = {
           "12th Grade in Science",
         duration:"2020 - 2022"
       },
-      {
-        title: "B.Tech",
-        subTitle: "Veer Surendra Sai University of technology, Burla",
-        description:
-          "Bachelor of technology in Information Technology",
-        duration:"2022 - 2026"
-      },
+      
     ],
   };
